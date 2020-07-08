62c866d37b749b616be6e3b4ee42345969d55ea1
shun <kaifenan@icloud.com>
2020-07-08 11:06:13 +0900

    add top page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 
 /**
@@ -9,16 +9,12 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+                                val components: ControllerComponents
+                              )
+  extends AbstractController(components)with I18nSupport {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index():Action[AnyContent] = Action { implicit request =>
     Ok(views.html.index())
   }
 }
